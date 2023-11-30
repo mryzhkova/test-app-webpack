de2c25e4341ec91318fbe3c3f657d9771030f266
Maryia Ryzhkova <MARyzhkova@alfabank.ru>
2023-11-30 15:18:29 +0300

    added webpack

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Link, Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import { AboutPageAsync } from "@/pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "@/pages/MainPage/MainPage.async";
+import "./index.scss";
+
+export const App = () => {
+  return (
+    <div className="app">
+      <Link to="/">Main</Link>
+      <Link to="/about">About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
